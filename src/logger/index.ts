db4d9208e08b9a0d1ae70d5f5a745f1f69ebf346
Dmiriy Rusov <rusov.d.s@admon.pro>
2020-08-06 08:48:11 +0700

    add log debug level

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,16 +1,23 @@
-import { createLogger, format, transports, Logger }  from 'winston'
-const { combine, timestamp, label, printf } = format
+import {
+  createLogger, format, transports, Logger
+} from 'winston'
 
-export default function(labelValue: string): Logger{
-  const myFormat = printf(({ level, message, label, timestamp }) => {
-    return `${timestamp} [${label}] ${level}: ${JSON.stringify(message)}`
-  })
+const {
+  combine, printf
+} = format
+
+export default (labelValue: string): Logger => {
+  const myFormat = printf(({
+    level, message, label, timestamp
+  }) =>
+    `${timestamp} [${label}] ${level}: ${JSON.stringify(message)}`)
 
   const logger = createLogger({
-    format: combine(format.colorize(), label({ label: labelValue }), timestamp(), myFormat),
+    level: process.env.DEBUG ? 'debug' : undefined,
+    format: combine(format.colorize(), format.label({ label: labelValue }), format.timestamp(), myFormat),
     transports: [new transports.Console()],
     silent: process.env.NODE_ENV === 'test'
   })
-  
+
   return logger
-} 
+}
